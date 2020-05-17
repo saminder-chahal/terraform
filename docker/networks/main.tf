1d5cb236733f0351a663d101a227a9caa9606e5b
Saminderjit, Chahal <cloud_user@schahal131c.mylabserver.com>
2020-05-17 20:12:46 +0000

    Adding docker swarm changes

--- docker/networks/main.tf
+++ docker/networks/main.tf
@@ -4,6 +4,10 @@ resource "docker_container" "mysql_container" {
   env   = [
     "MYSQL_ROOT_PASSWORD=${var.mysql_root_password}"
   ]
+  volumes {
+    volume_name    = "${docker_volume.mysql_data_volume.name}"
+    container_path = "/var/lib/mysql"
+  }
   networks_advanced {
     name    = "${docker_network.private_bridge_network.name}"
     aliases = ["${var.mysql_network_alias}"]
---
+++ docker/services/README.md
@@ -0,0 +1,3 @@
+Uses:
+1. Overlay network for swarm service
+2. Creates a docker service (assuming host machine is part of the swarm)
